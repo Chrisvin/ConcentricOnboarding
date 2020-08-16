97f07fc2618eca038e7734586e11dc15c361cef4
Jem <chrisvin1997@gmail.com>
2020-08-17 04:21:58 +0530

    Add previous button

--- app/src/main/java/com/example/concentriconboarding/MainActivity.kt
+++ app/src/main/java/com/example/concentriconboarding/MainActivity.kt
@@ -19,17 +19,27 @@ class MainActivity : AppCompatActivity() {
         // Definitely not a good idea for production, but good enough for a demo app.
         viewpager.setCurrentItem(titleArray.count() * 10, false)
 
-        nextButton.post {
-            viewpager.revealCenterPoint =
-                PointF(
-                    nextButton.x,
-                    nextButton.y + nextButton.height / 2
-                )
-            viewpager.revealRadius = nextButton.width / 2
+        previousButton.setOnClickListener {
+            updateViewPagerReveal(
+                centerX = previousButton.x + previousButton.width / 2,
+                centerY = previousButton.y + previousButton.height / 2,
+                radius = previousButton.width / 2
+            )
+            viewpager.setCurrentItem(viewpager.currentItem - 1, true)
         }
 
         nextButton.setOnClickListener {
+            updateViewPagerReveal(
+                centerX = nextButton.x + nextButton.width / 2,
+                centerY = nextButton.y + nextButton.height / 2,
+                radius = nextButton.width / 2
+            )
             viewpager.setCurrentItem(viewpager.currentItem + 1, true)
         }
+    }
+
+    private fun updateViewPagerReveal(centerX: Float, centerY: Float, radius: Int) {
+        viewpager.revealCenterPoint = PointF(centerX, centerY)
+        viewpager.revealRadius = radius
     }
 }
