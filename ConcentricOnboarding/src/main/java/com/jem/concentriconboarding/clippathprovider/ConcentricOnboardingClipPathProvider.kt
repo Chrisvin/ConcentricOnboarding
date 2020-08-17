2de4d773b4866255e1742d9329cf6f2a80849674
Jem <chrisvin1997@gmail.com>
2020-08-17 06:17:06 +0530

    Add initial/final circle growing/shrinking
    Fixes initial/final circle abruptness.

--- ConcentricOnboarding/src/main/java/com/jem/concentriconboarding/clippathprovider/ConcentricOnboardingClipPathProvider.kt
+++ ConcentricOnboarding/src/main/java/com/jem/concentriconboarding/clippathprovider/ConcentricOnboardingClipPathProvider.kt
@@ -5,6 +5,7 @@ import android.graphics.PointF
 import android.graphics.Region
 import android.view.View
 import com.jem.concentriconboarding.base.ClipPathProvider
+import com.jem.concentriconboarding.util.MathUtil.convertValueUsingRange
 import kotlin.math.max
 import kotlin.math.pow
 
@@ -15,6 +16,7 @@ class ConcentricOnboardingClipPathProvider : ClipPathProvider() {
 
     private val BASE_RADIUS = 0
     private val LIMIT = 50
+    private val INITIAL_CIRCLE = 5
 
     var radius = BASE_RADIUS
     var centerPoint = PointF(Float.MIN_VALUE, Float.MIN_VALUE)
@@ -35,10 +37,25 @@ class ConcentricOnboardingClipPathProvider : ClipPathProvider() {
         }
     }
 
-    private fun getRightCirclePath(progress: Float, view: View): Path {
-        if (progress < 0.01f) {
+    private fun getRightCirclePath(originalProgress: Float, view: View): Path {
+        if (originalProgress < 0.01f) {
             return Path()
         }
+        if (originalProgress < INITIAL_CIRCLE) {
+            return Path().apply {
+                addCircle(
+                    /*x = */centerPoint.x,
+                    /*y = */centerPoint.y,
+                    /*radius = */radius * originalProgress * (1f / INITIAL_CIRCLE),
+                    Path.Direction.CW
+                )
+            }
+        }
+        val progress = convertValueUsingRange(
+            originalProgress,
+            INITIAL_CIRCLE.toFloat(), LIMIT.toFloat(),
+            0f, LIMIT.toFloat()
+        )
         val r = radius + 1.25.pow(progress.toDouble())
             .coerceAtMost(max(view.width, view.height).toDouble() * 100)
         val delta = ((1 - progress / LIMIT) * radius)
@@ -52,10 +69,25 @@ class ConcentricOnboardingClipPathProvider : ClipPathProvider() {
         }
     }
 
-    private fun getLeftCirclePath(progress: Float, view: View): Path {
-        if (progress > LIMIT - 0.01f) {
+    private fun getLeftCirclePath(originalProgress: Float, view: View): Path {
+        if (originalProgress > LIMIT - 0.01f) {
             return Path()
         }
+        if (originalProgress > LIMIT - INITIAL_CIRCLE) {
+            return Path().apply {
+                addCircle(
+                    /*x = */centerPoint.x,
+                    /*y = */centerPoint.y,
+                    /*radius = */radius * (LIMIT - originalProgress) * (1f / INITIAL_CIRCLE),
+                    Path.Direction.CW
+                )
+            }
+        }
+        val progress = convertValueUsingRange(
+            originalProgress,
+            0f, LIMIT.toFloat() - INITIAL_CIRCLE,
+            0f, LIMIT.toFloat()
+        )
         val r = radius + 1.25.pow((LIMIT - progress).toDouble())
             .coerceAtMost(max(view.width, view.height).toDouble() * 100)
         val delta = (progress / LIMIT) * radius
